6a1d58f82feb16a6d7383db941e503cb57ea9e75
Fazle Rabbi <you@example.com>
2023-10-23 00:32:53 +0600

    Dockerfile added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
